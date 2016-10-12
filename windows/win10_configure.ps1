4c58fa6681e5a58fed1bfd6109a0d9b8cdd0ab70
Eldon Ferran de Pol <eldon@sensemaking.uk.com>
2016-10-12 22:34:26 +0100

    _

--- windows/win10_configure.ps1
+++ windows/win10_configure.ps1
@@ -1,1 +1,25 @@
-choco install NetFx3 -s windowsfeatures
+Disable-UAC
+Disable-GameBarTips
+Disable-MicrosoftUpdate
+Disable-BingSearch
+
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions -EnableShowFullPathInTitleBar
+Set-TaskbarOptions -Unlock -AutoHide -Dock Bottom -Combine Always
+$key = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
+Set-ItemProperty $key DontUsePowerShellOnWinX 0
+Set-ItemProperty $key ShowTaskViewButton 0
+Stop-Process -ProcessName explorer -Force
+
+choco uninstall MicrosoftWindowsPowerShellV2Root -s windowsfeatures -yr
+choco uninstall NetFx4-AdvSrvs -s windowsfeatures -yr
+choco uninstall Printing-Foundation-Features -s windowsfeatures -yr
+choco uninstall Printing-PrintToPDFServices-Features -s windowsfeatures -yr
+choco uninstall Printing-XPSServices-Features -s windowsfeatures -yr
+choco uninstall Xps-Foundation-Xps-Viewer -s windowsfeatures -yr
+choco uninstall MSRDC-Infrastructure -s windowsfeatures -yr
+choco uninstall SMB1Protocol -s windowsfeatures -yr
+choco uninstall WorkFolders-Client -s windowsfeatures -yr
+choco uninstall MediaPlayback -s windowsfeatures -yr
+
+
+
--- windows/win10_install.ps1
+++ windows/win10_install.ps1
@@ -1,5 +1,8 @@
+install-windowsUpdate -acceptEula
+
 choco install googlechrome -yr
+ & "${env:programFiles(x86)}\Google\Chrome\Application\chrome.exe" --make-default-browser
 choco install skype -yr
 choco install sublimetext3 -yr
 choco install visualstudio2015community -yr
-choco install sqlserver2014express -yr
+choco install mssqlserver2014express -yr
