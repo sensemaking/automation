e98316672ee5a1e485353ebc1f55494a6a8610dc
Eldon Ferran de Pol <eldon@sensemaking.uk.com>
2016-10-15 14:02:15 +0100

    _

--- windows/win10_install.ps1
+++ windows/win10_install.ps1
@@ -1,8 +1,8 @@
 install-windowsUpdate -acceptEula
 
-choco install googlechrome -yr
- & "${env:programFiles(x86)}\Google\Chrome\Application\chrome.exe" --make-default-browser
-choco install skype -yr
-choco install sublimetext3 -yr
-choco install visualstudio2015community -yr
 choco install mssqlserver2014express -yr
+choco install nodejs -yr
+choco install visualstudiocode -yr
+choco install postman -yr
+choco install googlechrome -yr
+choco install skype -yr
