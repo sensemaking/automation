cec55844465e2f4296b53a77bd1682c9e1f367b2
Eldon Ferran de Pol <eldon@sensemaking.uk.com>
2017-04-18 20:34:16 +0100

    --no-status

--- windows/powershell/AddEnvironmentPaths.ps1
+++ windows/powershell/AddEnvironmentPaths.ps1
@@ -3,8 +3,7 @@ $paths =
 	"${env:programFiles(x86)}\Google\Chrome\Application\", 
 	"$env:winDir\System32\inetsrv", 
 	"$env:USERPROFILE\AppData\Local\slack\",
-	"$env:programFiles\PostgreSQL\9.6\bin",
-	"$env:programFiles\PostgreSQL\9.6\pgAdmin 4\bin"
+	"${env:programFiles(x86)}\Program Files (x86)\TeamViewer"
 
 $paths | % {
 	if(-not (($env:path -split ';') -contains $_)) {
