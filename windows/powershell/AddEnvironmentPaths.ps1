15f0977e083209b967b41960b109cdf9e175752f
Eldon Ferran de Pol <eldon@sensemaking.uk.com>
2017-04-18 20:54:42 +0100

    --no-status

--- windows/powershell/AddEnvironmentPaths.ps1
+++ windows/powershell/AddEnvironmentPaths.ps1
@@ -3,6 +3,7 @@ $paths =
 	"${env:programFiles(x86)}\Google\Chrome\Application\", 
 	"$env:winDir\System32\inetsrv", 
 	"$env:USERPROFILE\AppData\Local\slack\",
+	"${env:programFiles(x86)}\Skype\Phone",
 	"${env:programFiles(x86)}\TeamViewer"
 
 $paths | % {
