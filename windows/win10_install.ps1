b88b866a963595aecb497b31ccf9c1e207b3b0f5
Ferran De Pol <eferrandepol@Companynet.org>
2017-07-11 09:47:14 +0100

    --no-status

--- windows/win10_install.ps1
+++ windows/win10_install.ps1
@@ -1,7 +1,5 @@
-choco install googlechrome -yr
 choco install slack -yr
-choco install skype -yr
-choco install teamviewer -yr
+choco install visualstudio2017community
 choco install visualstudiocode -yr
 choco install nodejs -yr
 choco install postman -yr
