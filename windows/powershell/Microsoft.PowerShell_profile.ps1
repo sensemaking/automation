00cfbf04c0a0e126772569e1e89cd49fc5eb3704
Eldon Ferran De Pol <eferrandepol@hearst.com>
2017-08-02 10:57:13 +0100

    _

--- windows/powershell/Microsoft.PowerShell_profile.ps1
+++ windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -6,7 +6,7 @@ import-module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 & $PSScriptRoot\AddEnvironmentPaths.ps1
 & $PSScriptRoot\Projects.ps1
 
-(get-projects).GetEnumerator() | Where { [bool]$_.Value.Script } |  % { import-module $_.Value.Script }
+(get-projects).GetEnumerator() | Where { [bool]$_.Value.Script } |  % { import-module $_.Value.Script -DisableNameChecking }
 
 function Edit-Profile {
     code (Split-Path $PROFILE)
