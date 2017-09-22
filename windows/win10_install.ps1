71721df2e855855355ade5a510da9d2fb98dee78
eldonferranpol <eldon@sensemaking.uk.com>
2017-09-22 08:26:24 +0100

    Update win10_install.ps1

--- windows/win10_install.ps1
+++ windows/win10_install.ps1
@@ -7,5 +7,7 @@ choco install nodejs -yr
 choco install postman -yr
 choco install sql-server-express -yr
 choco install sql-server-management-studio -yr
+choco install nuget.commandline -yr
+choco install nunit -yr
 
 Restart-Computer
