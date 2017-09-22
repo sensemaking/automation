e4bfefdfe27033c83c77a5f3007c539d8050469b
eldonferranpol <eldon@sensemaking.uk.com>
2017-09-22 08:22:58 +0100

    Update Projects.ps1

--- windows/powershell/Projects.ps1
+++ windows/powershell/Projects.ps1
@@ -42,14 +42,41 @@ function global:Open ([Project] $project){
 function global:Build ([Project] $project){
     function Build-Project($targetProject){
         if ($_.Value.VsSolution -ne $null) { 
+            Write-Host `nBuilding $targetProject.Key `n -Fore Green
             nuget.exe restore (Resolve-Path ($_.Value.VsSolution))
             MSBuild.exe (Resolve-Path ($_.Value.VsSolution)) /p:Config=Release /v:quiet
-            $migrationPath = (Split-Path($_.Value.VsSolution)) + "\Migrations"
-            if(Test-Path($migrationPath)){
-                & (Resolve-Path $migrationPath\bin\Run.exe)
+        } 
+    }
+
+    (Get-Projects).GetEnumerator() | Where-Object { $project.HasFlag($_.Key) } | % { Build-Project $_ }
+    Migrate $project
+    Test $project
+}
+
+function global:Migrate ([Project] $project){
+    function Migrate-Project($targetProject){
+        if ($_.Value.VsSolution -ne $null) { 
+            $solutionPath = (Split-Path($_.Value.VsSolution))
+            if(Test-Path("$solutionPath\Migrations")){
+                Write-Host `nRunning Migrations $targetProject.Key `n -Fore Green
+                & (Resolve-Path $solutionPath\Migrations\bin\Run.exe)
             }
         } 
     }
 
-    (Get-Projects).GetEnumerator() | Where{ $project.HasFlag($_.Key) } | % { Build-Project $_ }
+    (Get-Projects).GetEnumerator() | Where-Object { $project.HasFlag($_.Key) } | % { Migrate-Project $_ }
+}
+
+function global:Test ([Project] $project){
+    function Test-Project($targetProject){
+        if ($_.Value.VsSolution -ne $null) { 
+            Write-Host `nRunning Tests $targetProject.Key `n -Fore Green
+            $dir = Get-Location
+            Set-Location $_.Value.Directory
+            nunit3-console.exe (Get-ChildItem *Testing*.dll -Recurse | Where-Object { $_.FullName -notlike '*obj*' }) --noresult
+            Set-Location $dir
+        } 
+    }
+
+    (Get-Projects).GetEnumerator() | Where-Object { $project.HasFlag($_.Key) } | % { Test-Project $_ }
 }
