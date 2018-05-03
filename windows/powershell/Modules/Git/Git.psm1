da8583de7a96fcc227aab3d40e6e691ab9b4eb54
Chris Meek <cmeek@hearst.com>
2018-05-03 11:50:27 +0100

    _

--- windows/powershell/Modules/Git/Git.psm1
+++ windows/powershell/Modules/Git/Git.psm1
@@ -1,29 +1,28 @@
 function Ssh-SignIn{
     Stop-Process -Name 'ssh-agent' -ErrorAction SilentlyContinue
         
-    & "ssh-agent.exe" | % {
+    & "$env:programFiles\Git\usr\bin\ssh-agent.exe" | % {
         if ($_ -match '(?<key>[^=]+)=(?<value>[^;]+);') {
             [void][Environment]::SetEnvironmentVariable($Matches['key'], $Matches['value'])
         }
     }
 
-    & "ssh-add.exe" $env:USERPROFILE\.ssh\sm_rsa
+    & "$env:programFiles\Git\usr\bin\ssh-add.exe" $env:USERPROFILE\.ssh\sm_rsa
 }
 
 function Status([Project] $project = [Project]::All){
     function Show-Status($targetProject){
         Write-Host `nStatus for $targetProject.Key -Fore Green
         Set-Location $targetProject.Value.Directory
-        git status . -s
+        git status .
     } 
 
     $dir = Get-Location
     (Get-Projects).GetEnumerator() | Where-Object { $project.HasFlag($_.Key) } | % { Show-Status $_ }
     Set-Location $dir
-    Write-Host `n
 }
 
-function Pull ([Project] $project = [Project]::None){
+function Pull ([Project] $project = [Project]::All){
     function Git-Pull($targetProject){
         Write-Host `nPulling $targetProject.Key -Fore Green
         Set-Location $targetProject.Value.Directory
@@ -37,12 +36,12 @@ function Pull ([Project] $project = [Project]::None){
 
 function Push ([Project] $project = [Project]::None, $message = "_"){
     function Git-Commit($targetProject){
-        Write-Host Committing $targetProject.Key -Fore Green
+        Write-Host `nCommitting $targetProject.Key -Fore Green
         Set-Location $targetProject.Value.Directory
         git pull
         git add .
-        git commit -m $message --no-status
-        git push
+        git commit . -m $message --no-status
+        git push 
     }
 
     $dir = Get-Location 
--- windows/powershell/Modules/Web/Web.psm1
+++ windows/powershell/Modules/Web/Web.psm1
@@ -1,10 +1,8 @@
 Import-Module C:\Windows\System32\WindowsPowerShell\v1.0\Modules\WebAdministration\WebAdministration.psd1
 
 $hostsFile = "C:\Windows\System32\drivers\etc\hosts" 
-function Add-Hosts($hostNames) {
-    $hostNames | % { 
-		"127.0.0.1`t" + "$_`n" | Add-Content -PassThru $hostsFile -Force | Out-Null 
-	}
+function Add-Host($hostName, $ip = "127.0.0.1") {
+    "$ip`t" + "$hostName" | Out-File $hostsFile -Append -Encoding ascii
 }
 
 function Add-Site($name, $port, $path, $hostName){
@@ -22,12 +20,15 @@ function Add-Site($name, $port, $path, $hostName){
 }
 
 function Enable-Tls($name, $hostName) {
-    New-SelfSignedCertificate -DnsName $hostName -CertStoreLocation "cert:\LocalMachine\My"
-    New-WebBinding -Name $name -IP "*" -Port 443 -Protocol https -HostHeader $hostName 
+    $port = 443
+    New-WebBinding -Name $name -IP "*" -Port $port -Protocol https -HostHeader $hostName 
 
-    $sslPath = "IIS:\SslBindings\!443!$hostName"
-    $cert = Get-Certificate($hostName)
-    New-Item -Path $sslPath -Value $cert 
+    if((Get-Certificate($hostName)) -eq $null){
+        New-SelfSignedCertificate -DnsName $hostName -CertStoreLocation "cert:\LocalMachine\My"
+        $sslPath = "IIS:\SslBindings\!443!$hostName"
+        $cert = Get-Certificate($hostName)
+        New-Item -Path $sslPath -Value $cert
+    } 
 }
 
 
@@ -35,10 +36,6 @@ function Get-Certificate($name) {
     return Get-ChildItem Cert:\LocalMachine\my | ? {$_.Subject -eq "CN=$name"} | Select -First 1
 }
 
-function Remove-Host($hostName) {
-   Get-Content $hostsFile | Select-String -Pattern $hostName -NotMatch | Out-File $hostsFile -Force
-}
-
-Export-ModuleMember -function Add-Hosts
+Export-ModuleMember -function Add-Host
 Export-ModuleMember -function Add-Site
 Export-ModuleMember -function Enable-Tls
