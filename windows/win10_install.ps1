6865f2554994ed657096ec3f6c3b6e23769597b9
Eldon Ferran de Pol <eldon@sensemaking.uk.com>
2020-04-05 17:35:02 +0100

    _

--- windows/win10_install.ps1
+++ windows/win10_install.ps1
@@ -1,11 +1,10 @@
 
+choco install brave -yr
 choco install 7zip -yr
-choco install visualstudio2017community -yr
+choco install visualstudio2019community -yr
 choco install visualstudiocode -yr
 choco install nodejs -yr
 choco install postman -yr
-choco install sql-server-express -yr
-choco install sql-server-management-studio -yr
 choco install nuget.commandline -yr
 choco install nunit -yr
 
