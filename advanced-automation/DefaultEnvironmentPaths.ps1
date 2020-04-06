13d5fac7edec63526b7907965f0f527a7b1097a7
Eldon Ferran de Pol <eldon@sensemaking.uk.com>
2020-04-06 09:58:16 +0100

    _

--- advanced-automation/DefaultEnvironmentPaths.ps1
+++ advanced-automation/DefaultEnvironmentPaths.ps1
@@ -1,15 +1,16 @@
 $paths = 
 	"$env:programFiles\Git\bin",
 	"$env:programFiles\Git\usr\bin",
-	"${env:programFiles(x86)}\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin",
-	"${env:programFiles(x86)}\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin",
 	"$env:programFiles\Microsoft VS Code",
 	"${env:programFiles(x86)}\Google\Chrome\Application\", 
+	"$env:USERPROFILE\AppData\Local\BraveSoftware\Brave-Browser\Application",
 	"$env:winDir\System32\inetsrv", 
 	"$env:USERPROFILE\AppData\Local\slack\",
-	"${env:programFiles(x86)}\nunit.org\nunit-console\",
+	"${env:programFiles(x86)}\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin",
 	"${env:programFiles(x86)}\TeamViewer",
-	"$env:programFiles\curl\bin"
+	"${env:programFiles(x86)}\nunit.org\nunit-console\",
+	"$env:USERPROFILE\AppData\Local\Postman",
+	"$env:programFiles\curl\bin",
 
 $paths | % {
 	if(-not (($env:path -split ';') -contains $_)) {
