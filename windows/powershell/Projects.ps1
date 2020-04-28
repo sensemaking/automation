76ba1633dad8d4d5a754c7b82e52b8c5f3bb6a37
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2020-04-28 12:15:06 +0100

    _

--- windows/powershell/Projects.ps1
+++ windows/powershell/Projects.ps1
@@ -5,7 +5,11 @@ public enum Project
     None = 0,
     Automation = 1,
     Common = 2,
-    Slumber = 4,
+    Persistence = 4,
+    Messaging = 8,
+    Web = 16,
+    Slumber = 32,
+    Core = 64,
     All = Automation + Common + Slumber
 }
 "@
@@ -14,6 +18,10 @@ function global:Get-Projects {
     return @{ 
         [Project]::Automation = [PSCustomObject]@{ Git = "git@github.com:sensemaking/automation.git"; Directory = "~\automation"; Script = $null; VsSolution = $null; CodeSolution = "~\automation"; HasJs = $false; };
         [Project]::Common = [PSCustomObject]@{ Git = "git@github.com:sensemaking/common.git"; Directory = "~\common"; Script = $null; VsSolution = "~\common\Common.sln"; CodeSolution = $null; HasJs = $false; };
+        [Project]::Core = [PSCustomObject]@{ Git = "git@github.com:sensemaking/core.git"; Directory = "~\core"; Script = $null; VsSolution = "~\core\Core.sln"; CodeSolution = $null; HasJs = $false; };
+        [Project]::Persistence = [PSCustomObject]@{ Git = "git@github.com:sensemaking/persistence.git"; Directory = "~\persistence"; Script = $null; VsSolution = "~\persistence\Persistence.sln"; CodeSolution = $null; HasJs = $false; };
+        [Project]::Messaging = [PSCustomObject]@{ Git = "git@github.com:sensemaking/messaging.git"; Directory = "~\messaging"; Script = $null; VsSolution = "~\messaging\Messaging.sln"; CodeSolution = $null; HasJs = $false; };
+        [Project]::Web = [PSCustomObject]@{ Git = "git@github.com:sensemaking/web.git"; Directory = "~\web"; Script = $null; VsSolution = "~\web\Web.sln"; CodeSolution = $null; HasJs = $false; };
         [Project]::Slumber = [PSCustomObject]@{ Git = "git@github.com:sensemaking/slumber.git"; Directory = "~\Slumber"; Script = $null; VsSolution = "~\Slumber\Slumber.sln"; CodeSolution = "~\Slumber\Host\Ui\Web\src"; HasJs = $true; };
     }
 }
