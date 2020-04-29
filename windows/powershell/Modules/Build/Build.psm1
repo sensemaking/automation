309e2fdf825802916a03a327faa3190ba55117b0
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2020-04-29 15:59:53 +0100

    _

--- windows/powershell/Modules/Build/Build.psm1
+++ windows/powershell/Modules/Build/Build.psm1
@@ -105,7 +105,7 @@ function Test ([Project] $project = [Project]::All){
         if ($_.Value.VsSolution -ne $null) { 
             Write-Host `nRunning .NET Tests $targetProject.Key `n -Fore Green
             Set-Location $_.Value.Directory
-            dotnet test (Get-ChildItem *Specs*.dll -Recurse | Where-Object { $_.FullName -notlike '*obj*' -and $_.FullName -notlike '*Builders*'}) --noresult --noheader
+            dotnet test (Get-ChildItem *Specs*.dll -Recurse | Where-Object { $_.FullName -notlike '*obj*' -and $_.FullName -notlike '*Builders*'}) 
             BreakOnFailure $dir 'Testing Failed'
         }
         
