88c100c2e1b727aace1666f5a951e52e3928c735
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2020-05-22 16:14:24 +0100

    _

--- windows/powershell/Modules/Web/Web.psm1
+++ windows/powershell/Modules/Web/Web.psm1
@@ -1,23 +1,27 @@
 Import-Module C:\Windows\System32\WindowsPowerShell\v1.0\Modules\WebAdministration\WebAdministration.psd1
-$hostsFile = "C:\Windows\System32\drivers\etc\hosts" 
-
-function Add-Host($hostName, $ip = "127.0.0.1") {
-    "$ip`t" + "$hostName" | Out-File $hostsFile -Append -Encoding ascii
-}
 
 function Add-Site($name, $port, $path, $hostName){
+    #(Get-Content -path C:\Windows\System32\drivers\etc\hosts -Raw) -replace "127.0.0.1`tupredict.api.local`r`n",""
+    Remove-Host $name
     Remove-Website $name -ErrorAction SilentlyContinue
     Remove-WebAppPool $name -ErrorAction SilentlyContinue
     Remove-Certificate $name
+    
+    Add-Host $hostName
+    Add-AppPool $name
+    New-Website -Name $name -Port $port -PhysicalPath $path -ApplicationPool $name -HostHeader $hostName | Out-Null
+    Enable-Tls $name $hostName
+}
 
+function Add-Host($hostName, $ip = "127.0.0.1") {
+    "$ip`t$hostName" | Out-File C:\Windows\System32\drivers\etc\hosts -Append -Encoding ascii
+}
+
+function Add-AppPool($name) {    
     $appPool = New-WebAppPool $name -force
     $appPool.processModel.identityType = 0
     $appPool.managedRuntimeVersion = "v4.0"
     $appPool | Set-Item
-	
-    New-Website -Name $name -Port $port -PhysicalPath $path -ApplicationPool $name -HostHeader $hostName | Out-Null
-
-    Enable-Tls $name $hostName
 }
 
 function Enable-Tls($name, $hostName) {    
@@ -30,5 +34,4 @@ function Remove-Certificate($name) {
     Get-ChildItem Cert:\LocalMachine\my | ? {$_.Subject -eq "CN=$name"} | Remove-Item
 }
 
-Export-ModuleMember -function Add-Host
 Export-ModuleMember -function Add-Site
