b2174f93cae975237e7dd0f0d00277ab082f0b3c
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2020-05-22 15:31:37 +0100

    _

--- windows/powershell/Modules/Web/Web.psm1
+++ windows/powershell/Modules/Web/Web.psm1
@@ -6,10 +6,9 @@ function Add-Host($hostName, $ip = "127.0.0.1") {
 }
 
 function Add-Site($name, $port, $path, $hostName){
-    if(Test-Path IIS:\AppPools\$name) {
-        Remove-Website $name
-        Remove-WebAppPool $name
-    }
+    Remove-Website $name -ErrorAction SilentlyContinue
+    Remove-WebAppPool $name -ErrorAction SilentlyContinue
+    Remove-Certificate $name -ErrorAction SilentlyContinue
 
     $appPool = New-WebAppPool $name -force
     $appPool.processModel.identityType = 0
@@ -19,24 +18,15 @@ function Add-Site($name, $port, $path, $hostName){
     New-Website -Name $name -Port $port -PhysicalPath $path -ApplicationPool $name -HostHeader $hostName | Out-Null
 }
 
-function Enable-Tls($name, $hostName) {
-    $port = 443
-    $binding = New-WebBinding -Name $name -IP "*" -Port $port -Protocol https -HostHeader $hostName 
-
-    if((Get-Certificate($hostName)) -eq $null){
-        $cert = New-SelfSignedCertificate -DnsName $hostName -CertStoreLocation "cert:\LocalMachine\My"
-        $binding = Get-WebBinding -Name $name -Protocol https
-        $binding.AddSslCertificate($cert.GetCertHashString(), "my")
-        #$sslPath = "IIS:\SslBindings\!443!$hostName"
-        #$cert = Get-Certificate($hostName)
-        #if(!(Test-Path -Path $sslPath)) {
-        #    New-Item -Path $sslPath -Value $cert
-        #}
-    } 
+function Enable-Tls($name, $hostName) {    
+    New-WebBinding -Name $name -IP "*" -Port 443 -Protocol https -HostHeader $hostName 
+    $cert = New-SelfSignedCertificate -DnsName $hostName -CertStoreLocation "cert:\LocalMachine\My"
+    $binding = Get-WebBinding -Name $name -Protocol https
+    $binding.AddSslCertificate($cert.GetCertHashString(), "my")
 }
 
-function Get-Certificate($name) {
-    return Get-ChildItem Cert:\LocalMachine\my | ? {$_.Subject -eq "CN=$name"} | Select -First 1
+function Remove-Certificate($name) {
+    Get-ChildItem Cert:\LocalMachine\my | ? {$_.Subject -eq "CN=$name"} | Remove-Item
 }
 
 Export-ModuleMember -function Add-Host
