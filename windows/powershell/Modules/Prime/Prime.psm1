88d67f2b34fab863b631ccc956dcd84815d22398
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2020-06-01 12:02:41 +0100

    _

--- windows/powershell/Modules/Prime/Prime.psm1
+++ windows/powershell/Modules/Prime/Prime.psm1
@@ -1,6 +1,6 @@
 Import-Module C:\Windows\System32\WindowsPowerShell\v1.0\Modules\WebAdministration\WebAdministration.psd1
 
-function Add-LocalDbInstance($dbName){
+function Add-LocalDb($dbName){
     if((SqlLocalDB.exe info) -contains $dbName) {
         SqlLocalDB.exe stop $dbName 
         SqlLocalDB.exe delete $dbName 
