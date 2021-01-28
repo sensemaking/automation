f574806ebaa2a8e127bd909fda382cc961b930a4
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2021-01-28 14:34:34 +0000

    _

--- windows/chris@sensemaking.uk.com/VSCode/extensions.ps1
+++ windows/chris@sensemaking.uk.com/VSCode/extensions.ps1
@@ -2,4 +2,4 @@
 code --install-extension ms-vscode.powershell
 code --install-extension vscodevim.vim
 code --install-extension prettier
-code --install-extension vscode-jest
+code --install-extension orta.vscode-jest
