1d3faef103677c0774f610b0ba5dd780f7cd09ca
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2021-05-06 11:10:06 +0100

    _

--- windows/chris@sensemaking.uk.com/VSCode/extensions.ps1
+++ windows/chris@sensemaking.uk.com/VSCode/extensions.ps1
@@ -1,4 +1,5 @@
 
+code --install-extension geddski.macros 
 code --install-extension ms-vscode.powershell
 code --install-extension orta.vscode-jest
 code --install-extension dbaeumer.vscode-eslint
--- windows/eldon@sensemaking.uk.com/VSCode/extensions.ps1
+++ windows/eldon@sensemaking.uk.com/VSCode/extensions.ps1
@@ -1,5 +1,5 @@
 
+code --install-extension geddski.macros 
 code --install-extension ms-vscode.powershell
 code --install-extension vscodevim.vim
 code --install-extension dbaeumer.vscode-eslint
-code --install-extension geddski.macros 
