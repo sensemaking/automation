77429d897782506928368fd49ad48728de7c67ba
chris@sensemaking.uk.com <chris@sensemaking.uk.com>
2021-05-11 21:06:10 +0100

    _

--- windows/chris@sensemaking.uk.com/VSCode/extensions.ps1
+++ windows/chris@sensemaking.uk.com/VSCode/extensions.ps1
@@ -1,5 +1,6 @@
 
 code --install-extension geddski.macros 
+code --install-extension vscodevim.vim
 code --install-extension ms-vscode.powershell
 code --install-extension orta.vscode-jest
 code --install-extension dbaeumer.vscode-eslint
