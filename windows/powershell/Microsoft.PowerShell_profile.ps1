c2bacc89ee110dd129b9f16eea39eb519e7d17a1
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2022-01-18 11:14:11 +0000

    _

--- windows/powershell/Microsoft.PowerShell_profile.ps1
+++ windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -8,38 +8,37 @@ import-module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 & $PSScriptRoot\AddEnvironmentPaths.ps1
 & $PSScriptRoot\Projects.ps1
 
-(get-projects).GetEnumerator() | Where-Object { [bool]$_.Value.Script } |  % { import-module $_.Value.Script -DisableNameChecking }
-
-write-host "Does update work locally"
+(get-projects).GetEnumerator() | Where-Object { [bool]$_.Value.Script } | % { import-module $_.Value.Script -DisableNameChecking }
 
 function Edit-Profile { code (Split-Path $PROFILE) }
 
-function Edit-Hosts{ code c:\windows\system32\drivers\etc\hosts }
+function Edit-Hosts { code c:\windows\system32\drivers\etc\hosts }
 
 function Update-Automation { 
-  $automationDir =((get-projects).GetEnumerator() | Where-Object { $_.Name -eq "Automation" }).Value.Directory 
+  $automationDir = ((get-projects).GetEnumerator() | Where-Object { $_.Name -eq "Automation" }).Value.Directory 
   
-  if(!(Test-Path $automationDir)){ clone Automation }
+  if (!(Test-Path $automationDir)) { clone Automation }
 
   pull Automation
 
   Remove-Item "$(Split-Path $PROFILE)\Modules\*" -r -for
   Copy-Item "$automationDir\windows\powershell\modules" (Split-Path $PROFILE) -r -fo
+  Copy-Item "$automationDir\windows\powershell\AddEnvironmentPaths.ps1" -Destination "$profileDir\AddEnvironmentPaths.ps1" -fo
 
   Write-Host "`nUpdated automation scripts. Reloading shell`n" -Fore Green
   powershell.exe -nologo
 }
 
 function prompt {
-    $origLastExitCode = $LASTEXITCODE
+  $origLastExitCode = $LASTEXITCODE
 
-    $maxPathLength = 40
-    $curPath = $ExecutionContext.SessionState.Path.CurrentLocation.Path
-    if ($curPath.Length -gt $maxPathLength) {
-        $curPath = '...' + $curPath.SubString($curPath.Length - $maxPathLength + 3)
-    }
+  $maxPathLength = 40
+  $curPath = $ExecutionContext.SessionState.Path.CurrentLocation.Path
+  if ($curPath.Length -gt $maxPathLength) {
+    $curPath = '...' + $curPath.SubString($curPath.Length - $maxPathLength + 3)
+  }
 
-    Write-Host $curPath -ForegroundColor Green
-    $LASTEXITCODE = $origLastExitCode
-    "$('>' * ($nestedPromptLevel + 1)) "
+  Write-Host $curPath -ForegroundColor Green
+  $LASTEXITCODE = $origLastExitCode
+  "$('>' * ($nestedPromptLevel + 1)) "
 }
