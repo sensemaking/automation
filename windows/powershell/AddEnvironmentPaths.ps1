5d34b1f86ff95a877857d6d9585dac37e23b358d
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2022-05-04 09:35:48 +0100

    _

--- windows/powershell/AddEnvironmentPaths.ps1
+++ windows/powershell/AddEnvironmentPaths.ps1
@@ -1,16 +1,16 @@
 $paths = 
-	"${env:programFiles(x86)}\Google\Chrome\Application\", 
-	"$env:userprofile\AppData\Local\BraveSoftware\Brave-Browser\Application",
-	"${env:programFiles(x86)}\TeamViewer",
-	"$env:programFiles\curl\bin",
-	"$env:programFiles\Git\bin",
-	"$env:programFiles\Microsoft VS Code",
-	"${env:programFiles(x86)}\Microsoft Visual Studio\2019\Community\Common7\IDE",
-	"$env:programFiles\Azure Data Studio",
-	"$env:programFiles\Azure Cosmos DB Emulator"
+"${env:programFiles(x86)}\Google\Chrome\Application\", 
+"$env:programFiles\BraveSoftware\Brave-Browser\Application",
+"${env:programFiles(x86)}\TeamViewer",
+"$env:programFiles\curl\bin",
+"$env:programFiles\Git\bin",
+"$env:programFiles\Microsoft VS Code",
+"${env:programFiles(x86)}\Microsoft Visual Studio\2019\Community\Common7\IDE",
+"$env:programFiles\Azure Data Studio",
+"$env:programFiles\Azure Cosmos DB Emulator"
 
 $paths | % {
-	if(-not (($env:path -split ';') -contains $_)) {
+	if (-not (($env:path -split ';') -contains $_)) {
 		$env:path += ';' + $_
 	}
 }
