cfdf4e1e62cd075ede03b61ae3b8849850ffa746
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2022-10-03 17:01:23 +0100

    _

--- windows/powershell/Modules/Build/Build.psm1
+++ windows/powershell/Modules/Build/Build.psm1
@@ -1,37 +1,36 @@
 
-function GoTo ([Project] $project){
-    if($project -eq [Project]::None){
+function GoTo ([Project] $project) {
+    if ($project -eq [Project]::None) {
         Write-Host "`nNo project provided`n" -Fore Red
         return
     }
-    if($project -eq [Project]::All){
+    if ($project -eq [Project]::All) {
         Write-Host "`nCan't go to 'All' projects`n" -Fore Red
         return
     }
     Set-Location (Get-Projects).Get_Item($project).Directory
 }
 
-function Prime ([Project] $project){
-    function Prime-Project($targetProject){
-            if(-not(Test-Path "$($_.Value.Directory)")) { Clone $targetProject.key }
-            else{ pull $targetProject.Key }
+function Prime ([Project] $project) {
+    function Prime-Project($targetProject) {
+        if (-not(Test-Path "$($_.Value.Directory)")) { Clone $targetProject.key }
+        else { pull $targetProject.Key }
             
-            if(Test-Path "$($_.Value.Directory)\primer.ps1") {
-                Write-Host "`nRunning $($targetProject.Key) Primer`n" -ForegroundColor Green
-                & "$($_.Value.Directory)\primer.ps1"
-                Write-Host "`n$($targetProject.Key) Primed & Ready To Go`n" -ForegroundColor Green
-            }
-            else{ Write-Host "`n$($targetProject.Key) does not have a primer`n" -ForegroundColor Red }
+        if (Test-Path "$($_.Value.Directory)\primer.ps1") {
+            Write-Host "`nRunning $($targetProject.Key) Primer`n" -ForegroundColor Green
+            & "$($_.Value.Directory)\primer.ps1"
+            Write-Host "`n$($targetProject.Key) Primed & Ready To Go`n" -ForegroundColor Green
+        }
+        else { Write-Host "`n$($targetProject.Key) does not have a primer`n" -ForegroundColor Red }
 
-            Build $targetProject.Key
+        Build $targetProject.Key
     }
 
-    (Get-Projects).GetEnumerator() | Where{ $project.HasFlag($_.Key) } | % { Prime-Project $_ }
+    (Get-Projects).GetEnumerator() | Where { $project.HasFlag($_.Key) } | % { Prime-Project $_ }
 }
 
-function Open ([Project] $project = [Project]::None, [Switch] $frontEndOnly, [Switch] $backEndOnly){
-    function Open-Project($targetProject){
-        cls
+function Open ([Project] $project = [Project]::None, [Switch] $frontEndOnly, [Switch] $backEndOnly) {
+    function Open-Project($targetProject) {
         $dir = Get-Location
         Set-Location $_.Value.Directory     
         
@@ -42,7 +41,7 @@ function Open ([Project] $project = [Project]::None, [Switch] $frontEndOnly, [Sw
             & $_.Value.VsSolution 
         } 
 
-        if ($_.Value.CodeSolution -ne $null -and -not $backEndOnly) { 
+        if ($null -ne $_.Value.CodeSolution -and -not $backEndOnly) { 
             $dir = Get-Location
             Set-Location $_.Value.CodeSolution  
             code . 
@@ -51,18 +50,19 @@ function Open ([Project] $project = [Project]::None, [Switch] $frontEndOnly, [Sw
         Set-Location $dir
     }
 
-    (Get-Projects).GetEnumerator() | Where{ $project.HasFlag($_.Key) } | % { Open-Project $_ }
+    Clear-Host
+    (Get-Projects).GetEnumerator() | Where { $project.HasFlag($_.Key) } | % { Open-Project $_ }
 }
 
-function Build ([Project] $project = [Project]::All, [Switch] $frontEndOnly, [Switch] $backEndOnly){
-    function Build-Project($targetProject){
+function Build ([Project] $project = [Project]::All, [Switch] $frontEndOnly, [Switch] $backEndOnly) {
+    function Build-Project($targetProject) {
         $dir = Get-Location
         Set-Location $_.Value.Directory         
 
         git pull
         BreakOnFailure $dir '**************** Pull Failed ****************'
 
-        if ($_.Value.VsSolution -ne $null -and -not $frontEndOnly) { 
+        if ($null -ne $_.Value.VsSolution -and -not $frontEndOnly) { 
             Write-Host `nBuilding $targetProject.Key -Fore Green
             $solutionPath = Resolve-Path ($_.Value.VsSolution)            
             dotnet build $solutionPath --configuration Release -nologo --verbosity q --no-incremental 
@@ -88,9 +88,9 @@ function Build ([Project] $project = [Project]::All, [Switch] $frontEndOnly, [Sw
     Write-Host `n**************** Build was successful ****************`n -Fore Green
 }
 
-function Migrate ([Project] $project = [Project]::All){
-    function Migrate-Project($targetProject){
-        if ($_.Value.VsSolution -ne $null) { 
+function Migrate ([Project] $project = [Project]::All) {
+    function Migrate-Project($targetProject) {
+        if ($null -ne $_.Value.VsSolution) { 
             $dir = Get-Location
             Set-Location $_.Value.Directory
             Get-ChildItem .\ -Recurse | where { $_.Fullname -Like "*bin\Run.exe" } | % {      
@@ -106,8 +106,8 @@ function Migrate ([Project] $project = [Project]::All){
 }
     
 function Run-Client([Project] $project = [Project]::All) {
-    function Run($targetProject){
-        if ($_.Value.CodeSolution -ne $null) {
+    function Run($targetProject) {
+        if ($null -ne $_.Value.CodeSolution) {
             Write-Host `nRunning $targetProject.Key client `n -Fore Green     
             Set-Location $_.Value.CodeSolution    
             yarn start            
@@ -120,8 +120,8 @@ function Run-Client([Project] $project = [Project]::All) {
 
 function Run-Server([Project] $project = [Project]::All) {
     function Run($targetProject) {
-        if ($_.Value.ServerHost -ne $null) {
-            if(!(Get-Process CosmosDB.Emulator -ErrorAction SilentlyContinue)) { CosmosDB.Emulator.exe }
+        if ($null -ne $_.Value.ServerHost) {
+            if (!(Get-Process CosmosDB.Emulator -ErrorAction SilentlyContinue)) { CosmosDB.Emulator.exe }
             Write-Host `nRunning $targetProject.Key server `n -Fore Green
             Set-Location $_.Value.ServerHost
             dotnet watch run --urls https://localhost:5001
@@ -133,8 +133,8 @@ function Run-Server([Project] $project = [Project]::All) {
 }
 
 function Watch([Project] $project = [Project]::All) {
-    function Watch-Project($targetProject){
-        if ($_.Value.CodeSolution -ne $null) {
+    function Watch-Project($targetProject) {
+        if ($null -ne $_.Value.CodeSolution) {
             Write-Host `nWatching JavaScript $targetProject.Key `n -Fore Green     
             Set-Location $_.Value.CodeSolution    
             yarn watch
@@ -146,7 +146,7 @@ function Watch([Project] $project = [Project]::All) {
     (Get-Projects).GetEnumerator() | Where-Object { $project.HasFlag($_.Key) } | % { Watch-Project $_ }
 }
 
-function BreakOnFailure([string] $directory, [string] $message = 'It failed!'){
+function BreakOnFailure([string] $directory, [string] $message = 'It failed!') {
     if ($lastexitcode -ne 0) {
         Write-Host `n $message`n -Fore Red
         Set-Location $directory
