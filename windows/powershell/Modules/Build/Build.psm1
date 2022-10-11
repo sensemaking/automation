c90204a74c65b668efdef32ff8b8b154369c5e4d
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2022-10-11 16:58:52 +0100

    _

--- windows/powershell/Modules/Build/Build.psm1
+++ windows/powershell/Modules/Build/Build.psm1
@@ -121,7 +121,7 @@ function Run-Client([Project] $project = [Project]::All) {
 function Run-Server([Project] $project = [Project]::All) {
     function Run($targetProject) {
         if ($null -ne $_.Value.ServerHost) {
-            if (!(Get-Process CosmosDB.Emulator -ErrorAction SilentlyContinue)) { CosmosDB.Emulator.exe }
+            if (!(Get-Process CosmosDB.Emulator -ErrorAction SilentlyContinue)) { CosmosDB.Emulator.exe -NoUi }
             Write-Host `nRunning $targetProject.Key server `n -Fore Green
             Set-Location $_.Value.ServerHost
             dotnet watch run --urls https://localhost:5001
