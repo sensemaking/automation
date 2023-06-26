35c448e7d6f3b4af9bcf50f1d020d69e23f93f14
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2023-06-26 12:46:24 +0100

    _

--- windows/powershell/Projects.ps1
+++ windows/powershell/Projects.ps1
@@ -55,7 +55,7 @@ function global:Get-Projects {
             ServerHost="~\fdb-rx-authoring\Pharaoh\UI\server\host";
         };
         [Project]::RxCommon=[PSCustomObject]@{
-            Git="git@github.com:HearstHealthInternational/fdb-rx-authoring.git"; 
+            Git="git@github.com:HearstHealthInternational/fdb-rx-authoring-common.git"; 
             Directory="~\fdb-rx-authoring-common"; 
             VsSolution="\fdb-rx-authoring-common\Common.sln"; 
         };
