2e60610a9f817710c85c841571cc8d940ac01e39
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2023-06-30 15:16:56 +0100

    _

--- windows/powershell/Projects.ps1
+++ windows/powershell/Projects.ps1
@@ -10,82 +10,94 @@ public enum Project
     uPredict=16,
     Authoring=32,
     Publication=64,
-    Messaging=256,
+    Platform=128,
+    Rules=256,
+    Messaging=512,
     FdbPersistence=32768,
     Primitives=65536,
-    All=Automation + Core + Persistence + Web + uPredict + Authoring + Messaging + FdbPersistence + Primitives
+    All=Automation + Core + Persistence + Web + uPredict + Authoring + Publication + Platform + Rules + Messaging + FdbPersistence + Primitives
 }
 "@
 
 function global:Get-Projects {
     return @{ 
-        [Project]::Automation=[PSCustomObject]@{ 
-            Git="git@github.com:sensemaking/automation.git"; 
-            Directory="~\automation"; 
-            CodeSolution="~\automation"; 
+        [Project]::Automation     = [PSCustomObject]@{ 
+            Git          = "git@github.com:sensemaking/automation.git"; 
+            Directory    = "~\automation"; 
+            CodeSolution = "~\automation"; 
         };
-        [Project]::Core=[PSCustomObject]@{ 
-            Git="git@github.com:sensemaking/core.git"; 
-            Directory="~\core"; 
-            VsSolution="~\core\Core.sln"; 
+        [Project]::Core           = [PSCustomObject]@{ 
+            Git        = "git@github.com:sensemaking/core.git"; 
+            Directory  = "~\core"; 
+            VsSolution = "~\core\Core.sln"; 
         };
-        [Project]::Persistence=[PSCustomObject]@{ 
-            Git="git@github.com:sensemaking/persistence.git"; 
-            Directory="~\persistence"; 
-            VsSolution="~\persistence\Persistence.sln"; 
+        [Project]::Persistence    = [PSCustomObject]@{ 
+            Git        = "git@github.com:sensemaking/persistence.git"; 
+            Directory  = "~\persistence"; 
+            VsSolution = "~\persistence\Persistence.sln"; 
         };
-        [Project]::Web=[PSCustomObject]@{ 
-            Git="git@github.com:sensemaking/web.git"; 
-            Directory="~\web";
-            VsSolution="~\web\.net\Web.sln"; 
-            CodeSolution="~\web\js\core"; 
+        [Project]::Web            = [PSCustomObject]@{ 
+            Git          = "git@github.com:sensemaking/web.git"; 
+            Directory    = "~\web";
+            VsSolution   = "~\web\.net\Web.sln"; 
+            CodeSolution = "~\web\js\core"; 
         };        
-        [Project]::uPredict=[PSCustomObject]@{ 
-            Git="git@github.com:uPredict/uPredict.git"; 
-            Directory="~\uPredict"; 
-            VsSolution="~\uPredict\server\upredict.sln"; 
-            CodeSolution="~\uPredict\client"; 
-            ServerHost="~\uPredict\server\web\host"; 
+        [Project]::uPredict       = [PSCustomObject]@{ 
+            Git          = "git@github.com:uPredict/uPredict.git"; 
+            Directory    = "~\uPredict"; 
+            VsSolution   = "~\uPredict\server\upredict.sln"; 
+            CodeSolution = "~\uPredict\client"; 
+            ServerHost   = "~\uPredict\server\web\host"; 
         };
-        [Project]::Authoring=[PSCustomObject]@{
-            Git="git@github.com:HearstHealthInternational/fdb-rx-authoring.git"; 
-            Directory="~\fdb-rx-authoring"; 
-            VsSolution="~\fdb-rx-authoring\Authoring.sln"; 
-            CodeSolution="~\fdb-rx-authoring\Pharaoh\UI\client"; 
-            ServerHost="~\fdb-rx-authoring\Pharaoh\UI\server\host";
+        [Project]::Authoring      = [PSCustomObject]@{
+            Git          = "git@github.com:HearstHealthInternational/fdb-rx-authoring.git"; 
+            Directory    = "~\fdb-rx-authoring"; 
+            VsSolution   = "~\fdb-rx-authoring\Authoring.sln"; 
+            CodeSolution = "~\fdb-rx-authoring\Pharaoh\UI\client"; 
+            ServerHost   = "~\fdb-rx-authoring\Pharaoh\UI\server\host";
         };
-        [Project]::Publication=[PSCustomObject]@{
-            Git="git@github.com:HearstHealthInternational/fdb-rx-authoring-publication.git"; 
-            Directory="~\fdb-rx-authoring-publication"; 
-            VsSolution="~\fdb-rx-authoring-publication\Publication.sln"; 
+        [Project]::Publication    = [PSCustomObject]@{
+            Git        = "git@github.com:HearstHealthInternational/fdb-rx-authoring-publication.git"; 
+            Directory  = "~\fdb-rx-authoring-publication"; 
+            VsSolution = "~\fdb-rx-authoring-publication\Publication.sln"; 
         };
-        [Project]::FdbPersistence=[PSCustomObject]@{
-            Git="git@github.com:HearstHealthInternational/fdb-rx-persistence.git";
-            Directory="~\fdb-rx-persistence"; 
-            VsSolution="~\fdb-rx-persistence\Persistence.sln"; 
+        [Project]::Platform  = [PSCustomObject]@{
+            Git = "git@github.com:HearstHealthInternational/fdb-rx-platform-common.git" 
+            Directory = "~\fdb-rx-platform-common"; 
+            VsSolution = "~\fdb-rx-platform-common\Platform.Common.sln"; 
         };
-        [Project]::Messaging=[PSCustomObject]@{
-            Git="git@github.com:HearstHealthInternational/fdb-rx-messaging.git";
-            Directory="~\fdb-rx-messaging"; 
-            VsSolution="~\fdb-rx-messaging\Messaging.sln"; 
+        [Project]::Rules          = [PSCustomObject]@{
+            Git = "git@github.com:HearstHealthInternational/fdb-rx-rules.git" };
+            Directory = "~\fdb-rx-rules"; 
+            VsSolution = "~\fdb-rx-rules\Rules.sln"; 
         };
-        [Project]::Primitives=[PSCustomObject]@{
-            Git="git@github.com:HearstHealthInternational/fdb-rx-primitives.git";
-            Directory="~\fdb-rx-primitives";
-            VsSolution="~\fdb-rx-primitives\Primitives.sln";
+        [Project]::FdbPersistence = [PSCustomObject]@{
+            Git        = "git@github.com:HearstHealthInternational/fdb-rx-persistence.git";
+            Directory  = "~\fdb-rx-persistence"; 
+            VsSolution = "~\fdb-rx-persistence\Persistence.sln"; 
+        };
+        [Project]::Messaging      = [PSCustomObject]@{
+            Git        = "git@github.com:HearstHealthInternational/fdb-rx-messaging.git";
+            Directory  = "~\fdb-rx-messaging"; 
+            VsSolution = "~\fdb-rx-messaging\Messaging.sln"; 
+        };
+        [Project]::Primitives     = [PSCustomObject]@{
+            Git        = "git@github.com:HearstHealthInternational/fdb-rx-primitives.git";
+            Directory  = "~\fdb-rx-primitives";
+            VsSolution = "~\fdb-rx-primitives\Primitives.sln";
         };
     }
 }
 
 function global:Update-Projects { 
-   $automationDir =((get-projects).GetEnumerator() | where { $_.Name -eq "Automation" }).Value.Directory 
-   $profileDir=Split-Path $PROFILE
+    $automationDir = ((get-projects).GetEnumerator() | where { $_.Name -eq "Automation" }).Value.Directory 
+    $profileDir = Split-Path $PROFILE
    
-   if(!(Test-Path $automationDir)){ clone Automation }
+    if (!(Test-Path $automationDir)) { clone Automation }
 
-   pull Automation
+    pull Automation
  
-   Copy-Item "$automationDir\windows\powershell\Projects.ps1" -Destination "$profileDir\Projects.ps1" -fo
-   Write-Host "`nUpdated projects. Reloading shell`n" -Fore Green
-   powershell.exe -nologo
+    Copy-Item "$automationDir\windows\powershell\Projects.ps1" -Destination "$profileDir\Projects.ps1" -fo
+    Write-Host "`nUpdated projects. Reloading shell`n" -Fore Green
+    powershell.exe -nologo
 }
