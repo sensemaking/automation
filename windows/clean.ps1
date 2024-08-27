96108fce02779ac2585b105c60aa33c4e39b0b5b
Eldon Ferran de Pol <eldon@sensemaking.uk.com>
2024-08-27 14:17:35 +0100

    Update clean.ps1

--- windows/clean.ps1
+++ windows/clean.ps1
@@ -1,31 +1,4 @@
 
-winget uninstall onedrive
-
-Get-AppxPackage *3d* | Remove-AppxPackage -erroraction 'silentlycontinue'
-Get-AppxPackage *alarms* | Remove-AppxPackage -erroraction 'silentlycontinue'
-Get-AppxPackage *bing* | Remove-AppxPackage -erroraction 'silentlycontinue'
-Get-AppxPackage *camera* | Remove-AppxPackage -erroraction 'silentlycontinue'
-Get-AppxPackage *communi* | Remove-AppxPackage -erroraction 'silentlycontinue'
-Get-AppxPackage *candy* | Remove-AppxPackage -erroraction 'silentlycontinue'
-Get-AppxPackage *cortana* | Remove-AppxPackage -erroraction 'silentlycontinue'
-Get-AppxPackage *farm* | Remove-AppxPackage -erroraction 'silentlycontinue'
-Get-AppxPackage *feedback* | Remove-AppxPackage -erroraction 'silentlycontinue'
-Get-AppxPackage *getstarted* | Remove-AppxPackage -erroraction 'silentlycontinue'
-Get-AppxPackage *messaging* | Remove-AppxPackage -erroraction 'silentlycontinue'
-Get-AppxPackage *netflix* | Remove-AppxPackage -erroraction 'silentlycontinue'
-Get-AppxPackage *office* | Remove-AppxPackage -erroraction 'silentlycontinue'
-Get-AppxPackage *oneconnect* | Remove-AppxPackage -erroraction 'silentlycontinue'
-Get-AppxPackage *people* | Remove-AppxPackage -erroraction 'silentlycontinue'
-Get-AppxPackage *phone* | Remove-AppxPackage -erroraction 'silentlycontinue'
-Get-AppxPackage *skype* | Remove-AppxPackage -erroraction 'silentlycontinue'
-Get-AppxPackage *solit* | Remove-AppxPackage -erroraction 'silentlycontinue'
-Get-AppxPackage *soundrec* | Remove-AppxPackage -erroraction 'silentlycontinue'
-Get-AppxPackage *sticky* | Remove-AppxPackage -erroraction 'silentlycontinue'
-Get-AppxPackage *tunein* | Remove-AppxPackage -erroraction 'silentlycontinue'
-Get-AppxPackage *twitter* | Remove-AppxPackage -erroraction 'silentlycontinue'
-Get-AppxPackage *windowsmaps* | Remove-AppxPackage -erroraction 'silentlycontinue'
-Get-AppxPackage *xbox* | Remove-AppxPackage -erroraction 'silentlycontinue'
-Get-AppxPackage *zune* | Remove-AppxPackage -erroraction 'silentlycontinue'
 
 choco uninstall NetFx4-AdvSrvs -s windowsfeatures -yr
 choco uninstall Printing-Foundation-Features -s windowsfeatures -yr
@@ -40,6 +13,6 @@ choco uninstall MicrosoftWindowsPowerShellV2 -s windowsfeatures -yr
 choco uninstall Internet-Explorer-Optional-amd64 -s windowsfeatures -yr
 
 
-Write-Host "https://github.com/Raphire/Win11Debloat and https://github.com/ChrisTitusTech/win10script are of interest"
+Write-Host "Check out https://github.com/Raphire/Win11Debloat is still about. Run with custom options set"
 Read-Host "Computer will restart then please run .\automation\windows\install.ps1"
 Restart-Computer
