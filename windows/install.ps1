3f28045e381232d3fb5391497f98a1daf798e027
Eldon Ferran de Pol <eldon@sensemaking.uk.com>
2024-08-27 13:16:07 +0100

    Update install.ps1

--- windows/install.ps1
+++ windows/install.ps1
@@ -1,5 +1,4 @@
 
-choco install conemu -yr
 choco install 7zip -yr
 choco install slack -yr
 choco install teamviewer -yr
@@ -7,7 +6,6 @@ choco install dotnet-sdk -yr
 choco install jetbrains-rider
 choco install visualstudiocode -yr
 choco install sqllocaldb -yr
-choco install sql-server-2017-cumulative-update -yr
 choco install sqlserver-cmdlineutils -yr
 choco install azure-cosmosdb-emulator
 choco install azure-data-studio -yr
@@ -17,7 +15,6 @@ choco install nodejs -yr
 choco install yarn -yr
 
 yarn global add npm-check-updates
-yarn global add serve
 
 Read-Host "Computer will restart then please run .\automation\windows\configure.ps1"
 Restart-Computer
