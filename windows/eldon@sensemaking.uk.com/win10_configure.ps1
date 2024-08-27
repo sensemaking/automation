09bd4a26a9b8ef81c2409508c3625dd818bfb7d8
Eldon Ferran de Pol <eldon@sensemaking.uk.com>
2024-08-27 17:53:52 +0100

    Update win10_configure.ps1

--- windows/eldon@sensemaking.uk.com/win10_configure.ps1
+++ windows/eldon@sensemaking.uk.com/win10_configure.ps1
@@ -1,12 +1,1 @@
-choco install boxstarter
 
-import-module "$env:ALLUSERSPROFILE\Boxstarter\Boxstarter.Chocolatey"
-Disable-GameBarTips
-Disable-BingSearch
-
-Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowFileExtensions -EnableShowFullPathInTitleBar
-Set-BoxstarterTaskbarOptions -Unlock -Dock Bottom -Combine Always -Size Small
-$key = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
-Set-ItemProperty $key DontUsePowerShellOnWinX 0
-Set-ItemProperty $key ShowTaskViewButton 0
-Stop-Process -ProcessName explorer -Force
