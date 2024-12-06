416b3ceca8debbba608cd32be340a0d791d6e5f0
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2024-12-06 07:37:56 +0000

    _

--- windows/powershell/AddEnvironmentPaths.ps1
+++ windows/powershell/AddEnvironmentPaths.ps1
@@ -7,7 +7,7 @@ $paths =
 "$env:programFiles\Microsoft VS Code",
 "${env:programFiles(x86)}\Microsoft Visual Studio\2019\Community\Common7\IDE",
 "$env:programFiles\Azure Data Studio",
-"$env:programFiles\Azure Cosmos DB Emulator""387515a1-23ef-475d-ab49-2db3e69f6743"
+"$env:programFiles\Azure Cosmos DB Emulator"
 "$env:programFiles\C:\Users\eldon\AppData\Local\Livebook\Livebook.exe"
 
 $paths | % {
