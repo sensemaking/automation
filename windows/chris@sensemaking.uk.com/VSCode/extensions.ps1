1e2e5f31f2bfb0cc538d6ba45438916b7d0a7504
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2024-12-20 15:34:19 +0000

    _

--- windows/chris@sensemaking.uk.com/VSCode/extensions.ps1
+++ windows/chris@sensemaking.uk.com/VSCode/extensions.ps1
@@ -1,6 +1,6 @@
 
 code --install-extension geddski.macros 
-code --install-extension vscodevim.vim
 code --install-extension ms-vscode.powershell
-code --install-extension orta.vscode-jest
-code --install-extension dbaeumer.vscode-eslint
+code --install-extension vscodevim.vim
+code --install-extension jakebecker.elixir-ls
+code --install-extension oxc.oxc-vscode
