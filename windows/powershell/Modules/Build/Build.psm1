b7a17678d604fa0dee666943bc52733b6263dd9c
chris@sensemaking.uk.com <chris@sensemaking.uk.com>
2025-02-27 13:40:30 +0000

    _

--- windows/powershell/Modules/Build/Build.psm1
+++ windows/powershell/Modules/Build/Build.psm1
@@ -71,6 +71,7 @@ function Build ([Project] $project = [Project]::All, [Switch] $clientOnly, [Swit
         BreakOnFailure $dir '**************** Pull Failed ****************'
 
         if ($null -ne $_.Value.VsSolution -and -not $clientOnly) { 
+            Write-Host `nRestoring Nuget Packages $targetProject.Key -Fore Green
             dotnet restore
             Update-NuGet $project
             Write-Host `nBuilding $targetProject.Key -Fore Green
