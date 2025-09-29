891a968982b8a1d359598bc87ffea4b67d3898c2
ChristopherMeek <chris@sensemaking.uk.com>
2025-09-29 10:42:42 +0100

    _

--- windows/01-setup.ps1
+++ windows/01-setup.ps1
@@ -1,6 +1,8 @@
 $smRoot = read-host `nPlease enter the path you want to be the sensemaking root
 mkdir $smRoot -force
 
+sudo config --enable normal
+
 write-host "`nInstalling chocolatey" -fore yellow
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
