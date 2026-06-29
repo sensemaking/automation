a156d36a78c508cd8db11b2e2a055a6ad3e11dcd
Eldon Ferran de Pol <eldon@sensemaking.uk.com>
2026-06-29 15:47:31 +0100

    Update 03-install.ps1

--- windows/03-install.ps1
+++ windows/03-install.ps1
@@ -1,26 +1,26 @@
 
-choco install 7zip -yr
-choco install slack -yr
-choco install dotnet-sdk -yr
-choco innstall zoom -yr
+sudo choco install 7zip -yr
+sudo choco install slack -yr
+sudo choco install dotnet-sdk -yr
+sudo choco innstall zoom -yr
 
-choco install visualstudiocode -yr
-choco install sqllocaldb -yr
+sudo choco install visualstudiocode -yr
+sudo choco install sqllocaldb -yr
 
-choco install azure-data-studio -yr
-choco install azure-cli -yr
-choco install servicebusexplorer -yr
+sudo choco install azure-data-studio -yr
+sudo choco install azure-cli -yr
+sudo choco install servicebusexplorer -yr
 
-choco install nodejs -yr
-choco install pnpm -yr
+sudo choco install nodejs -yr
+sudo choco install pnpm -yr
 
-irm https://claude.ai/install.ps1 | iex
+sudo irm https://claude.ai/install.ps1 | iex
 
-refreshenv
+sudo refreshenv
 
-dotnet tool install --global dotnet-outdated-tool        
-pnpm add -g npm-check-updates
-pnpm add -g vite
+sudo dotnet tool install --global dotnet-outdated-tool        
+sudo pnpm add -g npm-check-updates
+sudo pnpm add -g vite
 
 Read-Host "Computer will restart then please run .\automation\windows\configure.ps1"
 Restart-Computer
